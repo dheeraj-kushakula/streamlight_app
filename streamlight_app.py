871c8f1d094447e06a3de974c062eeebcf5b9f90
dheeraj-kushakula <82694526+dheeraj-kushakula@users.noreply.github.com>
2023-04-03 18:26:54 -0400

    Update streamlight_app.py
    syntax error

--- streamlight_app.py
+++ streamlight_app.py
@@ -18,7 +18,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #put a pick list here so they can pick the fruit they want to 
-fruits_selected = streamlit.multiselect("Pick Some fruits:",list(my_fruit_list.index),['Avocado','strawberries'])
+fruits_selected = streamlit.multiselect("Pick Some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
